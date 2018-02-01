b701f408a2a6f151e1805c9f668f76f2b78a1487
Alvaro De La Torre <alvarodltp@gmail.com>
2018-02-01 14:42:40 -0600

    AlvaroDeLaTorre initial commit

---
+++ lib/alvarodelatorre_view_tool.rb
@@ -0,0 +1,5 @@
+require "alvarodelatorre_view_tool/version"
+
+module AlvarodelatorreViewTool
+  # Your code goes here...
+end
---
+++ lib/alvarodelatorre_view_tool/version.rb
@@ -0,0 +1,3 @@
+module AlvarodelatorreViewTool
+  VERSION = "0.1.0"
+end
